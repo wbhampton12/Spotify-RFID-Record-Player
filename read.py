1d14653465ae4c0cb3467528cb1d27b0f0cdd2e4
wbhampton12 <whampton@brew-meister.com>
2022-04-04 17:15:44 -0400

    Added some songs to list

--- read.py
+++ read.py
@@ -6,10 +6,10 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-        print("Waiting for you to scan an RFID sticker/card")
-        id = reader.read()[0]
-        print("The ID for this card is:", id)
+    print("Waiting for you to scan an RFID sticker/card")
+    id = reader.read()[0]
+    print("The ID for this card is:", id)
         
 finally:
-        GPIO.cleanup()
+    GPIO.cleanup()
         
